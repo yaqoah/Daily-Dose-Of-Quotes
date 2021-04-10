e366c10f7e6e843223361f59a9d19197715e3e5f
Ahmed Awad <balotelli-7amood@hotmail.com>
2021-04-11 00:29:35 +0100

    Changed Period

--- daily-dose-of-quotes/src/main/java/QuotesLauncher.java
+++ daily-dose-of-quotes/src/main/java/QuotesLauncher.java
@@ -11,7 +11,7 @@ public class QuotesLauncher {
 			animeNotification.showMessage();
 
 			try {
-				Thread.sleep(1000 * 15);
+				Thread.sleep(1000 * 60 * 20);
 			} catch (InterruptedException ex) {
 				System.out.println("user called quits");
 			}
